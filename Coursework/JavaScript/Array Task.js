d29cc7b61b616f3c7513865d468fa16c8e2aaf1e
TylerRTDev <tylerrtdev@outlook.com>
2024-07-23 15:22:50 +0100

    combine laptop files

---
+++ Coursework/JavaScript/Array Task.js
@@ -0,0 +1,13 @@
+var books = ["book1", "book2", "book3"];
+
+books.push("book4");
+
+
+let fruits = new Set();
+
+fruits.add("apples");
+fruits.add("oranges");
+fruits.add("bananas");
+fruits.delete("oranges");
+console.log(fruits);
+
